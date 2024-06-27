98bcb2e1faa503126e227062e07afb04becfa7e9
Bucky Roberts <bollamBrothers955@gmail.com>
2024-06-27 13:49:14 +0530

    first commit

---
+++ minimalistassignment-main/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from './components/BodyPart/BodyPart'
+
+function App() {
+  return (
+    <div className="home-img"> 
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
